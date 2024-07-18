d26b88b76f36630c54e9eaf0ffb7ae803b49da70
Atharva Todkary <100767857+AtharvaTodkary@users.noreply.github.com>
2024-07-18 13:01:37 +0530

    Add files via upload

--- Frontend/src/pages/CreditScore.js
+++ Frontend/src/pages/CreditScore.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import CreditCard from "../components/CreditCard";
-import FormCredit from "../components/FormCredit";
+// import FormCredit from "../components/FormCredit";
 
 export default function CreditScore() {
   return (
@@ -9,11 +9,11 @@ export default function CreditScore() {
       <Navigation />
       <div className="container bg-body-tertiary mt-5">
         <h1 className="fw-bolder text-center p-3">
-          Calculate Your <span className="text-info">Credit Scores</span>
+          Know Your <span className="text-info">Credit Scores</span>
         </h1>
         <hr />
         {/* Credit Score Form */}
-        <FormCredit />
+        {/* <FormCredit /> */}
         <div className="col-md-12 d-flex justify-content-evenly mt-5 pb-5">
           <CreditCard title="Credit Score" />
           <CreditCard title="Strategy" />
