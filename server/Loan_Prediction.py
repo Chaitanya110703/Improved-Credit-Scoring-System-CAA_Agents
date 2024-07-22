a1c4afef661abd87caa137f5c0cc0bffc6715dbd
Chaitanya110703 <dondbabasaheb655@gmail.com>
2024-07-22 14:05:38 +0530

    Changing The Loan Prediction model

--- server/Loan_Prediction.py
+++ server/Loan_Prediction.py
@@ -7,31 +7,23 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the model and scaler
-model = joblib.load('loan_prediction_2.joblib')
-scaler = joblib.load('loan_scaler_2.joblib')
-print("Model Loaded")
+model = joblib.load('loan_predict.joblib')
+
 
 def predict():
-    # Get the data from the POST request
+    
     data = request.get_json()
-    print("data")
-    # Extract features
+    
     features = np.array([
         data['dependants'],
-        data['cibilScore'],
-        data['loanTerm'],
-        data['education'],
-        data['selfEmployed'],
         data['income'],
-        data['loanAmount']
+        data['loanAmount'],
+        data['loanTerm'],
+        data['cibilScore'],
+        data['education'],
+        data['selfEmployed']
     ]).reshape(1, -1)
 
-    X_not_scale = features[:, [0, 1, 2, 3, 4]]
-    X_scaled = features[:, [5, 6]]
-
-    scaled_features = scaler.transform(X_scaled)
-
-    features = np.hstack([X_not_scale, scaled_features])
 
     prediction = model.predict(features)
 
